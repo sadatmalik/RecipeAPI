2faa4ed784a5d7442796ee8d13a6d02b279af8f4
sadat.malik <sadat.malik@me.com>
2021-11-30 14:20:08 +0000

    At caching annotations and logging to CRUD service methods

--- src/main/java/com/sadatmalik/recipeapi/services/ReviewService.java
+++ src/main/java/com/sadatmalik/recipeapi/services/ReviewService.java
@@ -6,7 +6,13 @@ import com.sadatmalik.recipeapi.exceptions.UserException;
 import com.sadatmalik.recipeapi.model.Recipe;
 import com.sadatmalik.recipeapi.model.Review;
 import com.sadatmalik.recipeapi.repositories.ReviewRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,21 +21,27 @@ import java.util.Optional;
 @Service
 public class ReviewService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReviewService.class);
     @Autowired
     ReviewRepo reviewRepo;
 
     @Autowired
     RecipeService recipeService;
 
+    @Cacheable(value = "reviewCache", key = "#id", sync = true)
     public Review getReviewById(Long id) throws NoSuchReviewException {
         Optional<Review> review = reviewRepo.findById(id);
 
         if (review.isEmpty()) {
             throw new NoSuchReviewException("The review with ID " + id + " could not be found.");
         }
+
+        LOGGER.info("Returning review from DB: " + review.get());
+
         return review.get();
     }
 
+    @Cacheable(value = "reviewListCache", key = "'recipeid:' + #recipeId", sync = true)
     public ArrayList<Review> getReviewByRecipeId(Long recipeId) throws NoSuchRecipeException, NoSuchReviewException {
         Recipe recipe = recipeService.getRecipeById(recipeId);
 
@@ -38,9 +50,13 @@ public class ReviewService {
         if (reviews.isEmpty()) {
             throw new NoSuchReviewException("There are no reviews for this recipe.");
         }
+
+        LOGGER.info("Looked up reviews for recipeId '" + recipeId + "': " + reviews);
+
         return reviews;
     }
 
+    @Cacheable(value = "reviewListCache", key = "'username:' + #username", sync = true)
     public ArrayList<Review> getReviewByUsername(String username) throws NoSuchReviewException {
         ArrayList<Review> reviews = reviewRepo.findByUser_Username(username);
 
@@ -48,9 +64,12 @@ public class ReviewService {
             throw new NoSuchReviewException("No reviews could be found for username " + username);
         }
 
+        LOGGER.info("Returning reviews for username '" + username + "' from DB: " + reviews);
+
         return reviews;
     }
 
+    @CachePut(value = "recipeCache", key = "#result.id")
     public Recipe postNewReview(Review review, Long recipeId) throws NoSuchRecipeException, UserException {
         Recipe recipe = recipeService.getRecipeById(recipeId);
         if (recipe.getUser().getUsername().equals(review.getUser().getUsername())) {
@@ -61,6 +80,10 @@ public class ReviewService {
         return recipe;
     }
 
+    @Caching(evict = {
+            @CacheEvict(value = "reviewCache", key = "#result.id"),
+            @CacheEvict(value = "reviewListCache", allEntries = true)
+    })
     public Review deleteReviewById(Long id) throws NoSuchReviewException {
         Review review = getReviewById(id);
 
@@ -68,9 +91,12 @@ public class ReviewService {
             throw new NoSuchReviewException("The review you are trying to delete does not exist.");
         }
         reviewRepo.deleteById(id);
+
         return review;
     }
 
+    @Caching(put = @CachePut(value = "reviewCache", key = "#result.id"),
+            evict = @CacheEvict(value = "recipeListCache", allEntries = true))
     public Review updateReviewById(Review reviewToUpdate) throws NoSuchReviewException {
         try {
             Review review = getReviewById(reviewToUpdate.getId());
@@ -78,7 +104,9 @@ public class ReviewService {
             throw new NoSuchReviewException("The review you are trying to update. Maybe you meant to create one? If not," +
                     "please double check the ID you passed in.");
         }
-        reviewRepo.save(reviewToUpdate);
-        return reviewToUpdate;
+
+        Review updatedReview = reviewRepo.save(reviewToUpdate);
+
+        return updatedReview;
     }
 }
